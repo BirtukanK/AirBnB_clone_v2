255bc4a05276675d19e249ecd0ba496bb4c315aa
Birtukan Kuma <birtukankuma1113@gmail.com>
2024-01-16 23:06:07 -0800

    setup mysql server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,5 +2,6 @@
 -- A databas and new user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
+Flush PRIVILEGES;
