3e3d462ab87cd6f5b98fe5e5a748a07ac3754b2c
BirtukanK <birtukankuma1113@gmail.com>
2024-02-01 10:20:26 -0800

    fix deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,15 +13,11 @@ def do_deploy(archive_path):
     no_tgz = "/data/web_static/releases/{}".format(filename.split('.')[0])
     # current = '/data/web_static/current'
     tmp = "/tmp/" + filename
-    try:
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}/".format(no_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("sudo rm -r {}".format(tmp))
-        run("sudo mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("sudo rm -rf {}/web_static".format(no_tgz))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}/ /data/web_static/current".format(no_tgz))
-        return True
-    except Exception:
-        return False
+    put(archive_path, "/tmp/")
+    run("sudo mkdir -p {}/".format(no_tgz))
+    run("sudo tar -xzf {} -C {}/".format(tmp, no_tgz))
+    run("sudo rm -r {}".format(tmp))
+    run("sudo mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+    run("sudo rm -rf {}/web_static".format(no_tgz))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {}/ /data/web_static/current".format(no_tgz))
