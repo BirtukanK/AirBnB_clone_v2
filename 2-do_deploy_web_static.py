2220f9f3c3377a30c91dd2f7eb30be3c71098659
BirtukanK <birtukankuma1113@gmail.com>
2024-02-01 09:31:10 -0800

    fix deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,11 +15,10 @@ def do_deploy(archive_path):
     tmp = "/tmp/" + filename
     try:
         put(archive_path, "/tmp/")
-        run("sudo rm -rf {}/".format(no_tgz))
         run("sudo mkdir -p {}/".format(no_tgz))
-        run("sudo tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
         run("sudo rm -r {}".format(tmp))
-        run("sudo mv {}/web_static/*" "{}/".format(no_tgz, no_tgz))
+        run("sudo mv {}/web_static/* {}/".format(no_tgz, no_tgz))
         run("sudo rm -rf {}/web_static".format(no_tgz))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {}/ /data/web_static/current".format(no_tgz))
