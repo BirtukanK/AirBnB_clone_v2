87680d570b9cfb3690fea3b593109e7fe4590f1b
BirtukanK <birtukankuma1113@gmail.com>
2024-02-01 05:13:06 -0800

    deploy web_static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Fabric script that distributes an archive to web servers"""
+from fabric.api import *
+from os.path import exists
+env.hosts = ['18.234.80.85', '52.87.216.243']
+
+def do_deploy(archive_path):
+    """ method that distributes an archive to both our servers"""
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = "/data/web_static/releases/{}".format(filename.split('.')[0])
+    # current = '/data/web_static/current'
+    tmp = "/tmp/" + filename
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("sudo rm -r {}".format(tmp))
+        run("sudo mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("sudo ln -s {}/ /data/web_static/current".format(no_tgz))
+        return True
+    except:
+        return False
