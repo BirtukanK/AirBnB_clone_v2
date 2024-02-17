2b1e161309603198e444fe2f636e01453dfa9448
BirtukanK <birtukankuma1113@gmail.com>
2024-02-17 16:13:49 +0000

    flask route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This defines flask"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Returns some text. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """ Returns some text. """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
